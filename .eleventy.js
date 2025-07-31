d9aec062fcea07f56847caf4a5745cc2631eed54
Zh4o <39034551+Zh4o@users.noreply.github.com>
2025-07-31 17:24:31 -0400

    working blog

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,16 @@
+// Add this line at the top of your .eleventy.js
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig) {
   // Passthrough copy for our assets
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("js");
   eleventyConfig.addPassthroughCopy("img");
+  
+  // Add the date filter
+  eleventyConfig.addFilter("dateString", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_FULL);
+  });
 
   return {
     dir: {
---
+++ _posts/my-first-post.md
@@ -0,0 +1,24 @@
+---
+title: My First Blog Post
+date: 2023-10-27
+layout: post.njk
+tags:
+  - post
+---
+
+This is my very first post on my new blog! Welcome.
+
+## Subheadings are easy
+
+You can use standard Markdown syntax.
+
+- Here is a list item.
+- And another one.
+
+Check out this code block:
+
+```javascript
+function hello() {
+  console.log("Hello, World!");
+}
+```
